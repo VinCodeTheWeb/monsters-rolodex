5ca9726da7c30e1268db528ddf5c70cf57d976e8
VinCodeTheWeb <vincenzo33.pellegrini@gmail.com>
2019-12-20 13:23:56 +0700

    Updates

---
+++ precache-manifest.56c76e647ad011e39060d943f6cd5d7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34fec71d7123947b4e4142bd3479b672",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5a7c87561e752b5c662f",
+    "url": "/monsters-rolodex/static/css/main.41e82345.chunk.css"
+  },
+  {
+    "revision": "617e0f92aa616ae4c4f4",
+    "url": "/monsters-rolodex/static/js/2.6fd2d76f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.6fd2d76f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a7c87561e752b5c662f",
+    "url": "/monsters-rolodex/static/js/main.c5a1cbd8.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
